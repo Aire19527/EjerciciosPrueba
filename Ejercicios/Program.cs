94370b1713970def4af0f57293fb08620d1c3964
Juan Pablo Rodriguez Toro <44555206+Aire19527@users.noreply.github.com>
2022-06-06 19:57:13 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicios/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicios_1 ObjEjercicio = new Ejercicios_1();
+            //ObjEjercicio.Md();
+
+            Ciclos objCiclos = new Ciclos();
+            objCiclos.Evaluacion_1();
+
+        }
+    }     
+}
